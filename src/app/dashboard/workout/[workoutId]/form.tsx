b5c946390d2ed14e1e21f35fded9e1e0ca387466
Bahaar Phekho <bahaar.phekho.2@gmail.com>
2026-06-08 17:18:08 -0400

    fix: remove initialFocus from Calendar

--- src/app/dashboard/workout/[workoutId]/form.tsx
+++ src/app/dashboard/workout/[workoutId]/form.tsx
@@ -66,7 +66,6 @@ export default function EditWorkoutForm({
                     setOpen(false);
                   }
                 }}
-                initialFocus
               />
             </PopoverContent>
           </Popover>
